cb3556eee09a0f22a4e221801ca0c6036a514a22
adibfirman <dib.firman@gmail.com>
2020-01-21 10:43:31 +0700

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+	env: {
+		browser: true,
+		es6: true
+	},
+	extends: ["plugin:react/recommended", "airbnb", "prettier"],
+	globals: {
+		Atomics: "readonly",
+		SharedArrayBuffer: "readonly"
+	},
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true
+		},
+		ecmaVersion: 2018,
+		sourceType: "module"
+	},
+	plugins: ["react", "@typescript-eslint", "react-hooks"],
+	rules: {
+		"react-hooks/rules-of-hooks": "error",
+		"react-hooks/exhaustive-deps": "warn",
+		"no-console": "off",
+		"react/jsx-filename-extension": [1, { extensions: [".js", ".jsx", ".tsx"] }]
+	}
+};
